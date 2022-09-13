51c1195bb16b18a799ca7d6d064869b78f70f76b
AnjuChowdary <anjuchowdary93@gmail.com>
2022-09-13 08:33:24 +0530

    Added Core splash library and splash screen

---
+++ app/src/main/java/com/ae/techsadha/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.ae.techsadha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.ViewTreeObserver
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import kotlinx.coroutines.delay
+
+class MainActivity : AppCompatActivity() {
+
+    var isReady: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            isReady = true
+        }, 6000)
+
+        //Use content draw listeners to do initial work
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener {
+            return@addOnPreDrawListener isReady
+        }
+    }
+}
