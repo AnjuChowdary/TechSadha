f6d33d23f06ec4bf7f89175f44b1067513c851e8
AnjuChowdary <anjuchowdary93@gmail.com>
2022-10-02 13:59:38 +0800

    Code clean, removed deprecated handler usage

--- app/src/main/java/com/ae/techsadha/MainActivity.kt
+++ app/src/main/java/com/ae/techsadha/MainActivity.kt
@@ -3,29 +3,29 @@ package com.ae.techsadha
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.view.View
-import android.view.ViewTreeObserver
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
 
-    var isReady: Boolean = false
+    private var isReady: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //To show splash screen for sometime, replace this with any other calls
-        Handler().postDelayed({
-            isReady = true
-        }, 6000)
-
+        //regionSplashScreen API To show splash screen for sometime, replace this with any other calls
+        Looper.myLooper()?.let {
+            Handler(it).postDelayed({
+                isReady = true
+            }, 6000)
+        }
         //Use content draw listeners to do initial work
         val content: View = findViewById(android.R.id.content)
         content.viewTreeObserver.addOnPreDrawListener {
             return@addOnPreDrawListener isReady
         }
+        //endregion
     }
 }
